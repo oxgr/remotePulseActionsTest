c870c5a0bb6adb3a6b3d725404a23786fc03703a
stephan schulz <stephan@antimodular.com>
2019-11-13 10:58:35 -0700

    - make heart beat object

---
+++ src/oneHeart.h
@@ -0,0 +1,167 @@
+//
+//  oneHeart.h
+//  remotePulse
+//
+//  Created by Stephan Schulz on 2019-11-12.
+//
+
+#ifndef oneHeart_h
+#define oneHeart_h
+
+#include "ofxTiming.h"
+
+class oneHeart {
+private:
+     ofxDmx * dmx;
+public:
+    
+    string myLabel;
+    int myIndex;
+    
+    ofParameter<bool> bUseSound;
+    
+    //BPM
+    int bpm;
+    int old_bpm;
+    
+    float bpmInSeconds;
+    float old_bpmInSeconds;
+    
+    
+    //__BPM
+    DelayTimer bpm_delayTimer;
+    LerpTimer bpm_lerpTimer;
+    
+  
+    
+    ofSoundPlayer  beat1;
+    ofSoundPlayer  beat2;
+    
+    bool bStart2ndBeat;
+
+    int dmxStartChannel;
+    
+    void setup(string _label, int _index, int _dmxStart, ofxDmx & dmx){
+        
+         this->dmx = &dmx;
+        myLabel = _label;
+        myIndex = _index;
+        dmxStartChannel = _dmxStart;
+        
+        bpm = 50;
+        
+        bpm_delayTimer.setPeriod(1); // to get it started
+        
+       
+        
+//        beat1.load("sounds/oneBeat.wav");
+//        beat2.load("sounds/oneBeat2.wav");
+        beat1.load("sounds/oneBeat3.wav");
+        beat2.load("sounds/oneBeat4.wav");
+        beat1.setVolume(1.0f);
+        beat2.setVolume(1.0f);
+        beat1.setMultiPlay(false);
+        beat2.setMultiPlay(false);
+        
+    }
+    
+    void update(bool _useDmx, float _beat2Offset){
+        
+        
+        
+        if(old_bpm != bpm){
+            old_bpm = bpm;
+            bpmInSeconds = 60.0 / float(bpm);
+            //            ofLog()<<"bpmInSeconds "<<bpmInSeconds;
+            //            bpm_delayTimer.setPeriod(bpmInSeconds);
+            //            bpm_lerpTimer.setDuration(bpmInSeconds);
+            
+        }
+        
+        
+        
+        if(bpm_delayTimer.tick() == true){
+            
+            bStart2ndBeat = true;
+            if(beat1.isPlaying() == false && bUseSound == true) beat1.play();
+//            ofLog()<<ofGetElapsedTimef()<<" beat1.play()";
+            
+            if(old_bpmInSeconds != bpmInSeconds){
+                old_bpmInSeconds = bpmInSeconds;
+                ofLog()<<"BPM "<<bpm<<" in bpmInSeconds "<<bpmInSeconds;
+                bpm_delayTimer.setPeriod(bpmInSeconds);
+                bpm_lerpTimer.setDuration(bpmInSeconds);
+            }
+            
+            bpm_lerpTimer.setToValue(0);
+            bpm_lerpTimer.lerpToValue(1);
+        }
+        
+        
+        //    ofLog()<<"bpm_lerpTimer.getProgress() "<<bpm_lerpTimer.getProgress()<<" bStart2ndBeat "<<bStart2ndBeat;
+        if(bpm_lerpTimer.getProgress() > _beat2Offset && bStart2ndBeat == true){
+            if(beat2.isPlaying() == false && bUseSound == true){
+                beat2.play();
+//                ofLog()<<ofGetElapsedTimef()<<" beat2.play()";
+                bStart2ndBeat = false;
+            }
+        }
+        
+        //    bpm_lerpTimer.lerpToValue(1);
+        
+       
+        
+        if(_useDmx){
+           
+            int temp_level_0 = 0;
+            
+            if(bpm_lerpTimer.getProgress() < 0.6){
+                temp_level_0 = ofMap(bpm_lerpTimer.getProgress(), 0, 0.6, 255, 0,true);
+                dmx->setLevel(0 + dmxStartChannel,temp_level_0);
+            }else{
+                dmx->setLevel(0 + dmxStartChannel,0);
+            }
+            
+            
+            int temp_level_1 = 0;
+            
+            if(bpm_lerpTimer.getProgress() > _beat2Offset && bpm_lerpTimer.getProgress() < 0.6){
+               temp_level_1 = ofMap(bpm_lerpTimer.getProgress(), _beat2Offset, 0.6, 255, 0,true);
+                 dmx->setLevel(1 + dmxStartChannel,temp_level_1);
+                
+//                ofLog()<<myLabel<<" temp_level_0 "<<temp_level_0<<" temp_level_1 "<<temp_level_1<<" bpm_lerpTimer.getProgress() "<<bpm_lerpTimer.getProgress();
+            }else{
+                 dmx->setLevel(1 + dmxStartChannel,0);
+            }
+        }
+        
+    }
+    void draw(int _x, int _y){
+        
+        ofPushMatrix();
+            ofTranslate(_x,_y);
+        
+        int tempY = 0;
+        ofSetColor(255);
+//        ofDrawBitmapString("meBPM "+ofToString(meBPM),0,tempY+=15);
+         ofDrawBitmapString(myLabel,0,tempY+=15);
+        ofDrawBitmapString("bpm "+ofToString(bpm),0,tempY+=15);
+       
+        
+        ofSetColor(255);
+        stringstream msg;
+        msg << "beat1.isPlaying: " << beat1.isPlaying() << endl;
+        msg << "beat2.isPlaying: " << beat2.isPlaying() << endl;
+        msg << "lerpTimer:"
+        << " from " << bpm_lerpTimer.getStartValue()
+        << " to " << bpm_lerpTimer.getTargetValue()
+        << " : " << int(bpm_lerpTimer.getValue())
+        << " (" << int(100*bpm_lerpTimer.getProgress()) << "%)" << endl;
+        ofDrawBitmapString(msg.str(),0,tempY+=15);
+        
+        ofPopMatrix();
+    }
+    
+    
+};
+#endif /* oneHeart_h */
